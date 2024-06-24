ea18ae2c5aa4e1e3675ba061fe0d54901cef01be
JiYong <35972643+bjy4824@users.noreply.github.com>
2024-06-24 19:36:23 +0900

    20240624

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ canvas.style.backgroundColor = "rgb(100,100,100)";
 var tableWidth = canvas.width * 0.7;
 var tableHeight = tableWidth * 0.5;
 var frameRate = 20;//계산당 프레임 1개의 비율
+var fricton_table = 0.0001;//테이블 마찰계수
 {
     //테이블 사이즈 중대 4구
     var table = {//당구대 내부
@@ -35,7 +36,7 @@ var frameRate = 20;//계산당 프레임 1개의 비율
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
             ctx.lineWidth = 2;
-            ctx.strokeRect(this.x, this.y, this.width, this.height); 
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
         }    
     }
     var dot = {//당구대 틀
@@ -256,6 +257,34 @@ var frameRate = 20;//계산당 프레임 1개의 비율
             ctx.fillRect(this.x, this.y, this.width, this.height); 
             ctx.lineWidth = 2;
             ctx.strokeRect(this.x, this.y, this.width, this.height); 
+           
+            ctx.beginPath(); 
+            ctx.strokeStyle = "#000000"; 
+            ctx.lineWidth = 2;
+            ctx.moveTo(this.x + this.width*0.25, this.y);
+            ctx.lineTo(this.x + this.width*0.25, this.y + this.height);
+            ctx.stroke();
+
+            ctx.beginPath(); 
+            ctx.moveTo(this.x + this.width*0.50, this.y);
+            ctx.lineTo(this.x + this.width*0.50, this.y + this.height);
+            ctx.stroke();
+
+            ctx.beginPath(); 
+            ctx.moveTo(this.x + this.width*0.75, this.y);
+            ctx.lineTo(this.x + this.width*0.75, this.y + this.height);
+            ctx.stroke();
+
+            ctx.font = "20px Georgia";
+            ctx.fillStyle = "#000000";
+            ctx.textAlign = "center";
+            ctx.fillText("100", this.x, this.y+this.height*1.4);
+            ctx.fillText("75", this.x+this.width*0.25, this.y+this.height*1.4);
+            ctx.fillText("50", this.x+this.width*0.50, this.y+this.height*1.4);
+            ctx.fillText("25", this.x+this.width*0.75, this.y+this.height*1.4);
+            ctx.fillText("0", this.x+this.width*1, this.y+this.height*1.4);
+            
+
         }    
     }
     
@@ -353,14 +382,13 @@ function systemDraw(){
     rightButton.draw();
     ballButton.draw();
 }
-systemDraw();
 //공 반지름
 var ballRadius = tableWidth / 2448 * 65.5 /2 
 var ball = {
     yellow : {
         color : "rgb(229, 197, 51)",
-        x : table.x + 0.25 * table.width,
-        y : table.y + 0.5 * table.height,
+        x : table.x + 0.25*table.width,//초기위치
+        y : table.y + 0.5*table.height,
         radius : ballRadius,
         angle : 0,
         //공의 속도 x,y축
@@ -372,27 +400,93 @@ var ball = {
         spinZ : 0,
         velocity(){
             return (Math.sqrt(this.velocityX**2 + this.velocityY**2));
-        },
-        draw(){
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-            ctx.fillStyle = this.color;
-            ctx.fill();
+        }
+    },
+    red_1 : {
+        color : "rgb(200, 40, 40)",
+        x : table.x + 0.75*table.width,//초기 위치
+        y : table.y + 0.5*table.height+ballRadius,//
+        radius : ballRadius,
+        angle : 0,
+        //공의 속도 x,y축
+        velocityX : 0,
+        velocityY : 0,
+        //공의 회전 축을 기준으로 컬
+        spinX : 0,
+        spinY : 0,
+        spinZ : 0,
+        velocity(){
+            return (Math.sqrt(this.velocityX**2 + this.velocityY**2));
         }
     }
 }
-ball.yellow.draw();
+
+//클릭구역 판단
+function isTable(x,y){//클릭한 구역이 테이블이면 true/아니면 false
+    if((x > table.x && x < table.x+table.width)&&(y > table.y && y < table.y+table.height)){
+        return true;
+    }
+    return false;
+}
+function isCuePower(x,y){//클릭한 구역이 큐파워이면 true/아니면 false
+    if((x > cuePower.x && x < cuePower.x+cuePower.width)&&(y > cuePower.y && y < cuePower.y+cuePower.height)){
+        return true;
+    }
+    return false;
+}
+function isLeft(x,y){//클릭한 구역이 레프트버튼이면 true/아니면 false
+    if((x > leftButton.x && x < leftButton.x+leftButton.width)&&(y > leftButton.y && y < leftButton.y+leftButton.height)){
+        return true;
+    }
+    return false;
+}
+function isRight(x,y){//클릭한 구역이 라이트버튼이면 true/아니면 false
+    if((x > rightButton.x && x < rightButton.x+rightButton.width)&&(y > rightButton.y && y < rightButton.y+rightButton.height)){
+        return true;
+    }
+    return false;
+}
+function isBall(x,y){//클릭한 구역이 시네루 버튼이면 true/아니면 false
+    if(((x-ballButton.x)**2 +(y-ballButton.y)**2) < ballButton.radius**2){
+        return true;
+    }
+    return false;
+}
+function reangle(x,y){//테이블 클릭시 공의 각도를 재지정
+    if((x-ball.yellow.x) > 0){//공의 오른쪽 클릭
+        ball.yellow.angle = Math.asin((y-ball.yellow.y)/Math.sqrt((x-ball.yellow.x)**2 + (y-ball.yellow.y)**2));
+    }
+    else{//공의 왼쪽 클릭
+        ball.yellow.angle = Math.asin(-(y-ball.yellow.y)/Math.sqrt((x-ball.yellow.x)**2 + (y-ball.yellow.y)**2))+Math.PI;
+    }
+}
+//상태 : ready=준비중(공 치는 준비)  play=게임 표현중(공 움직이는중)
+var state = "ready"
+//표현할 애니메이션 프레임(공들의 위치를 배열의 형태로 가짐)
+var frame = []
+function drawBalls([yellowX, yellowY, red_1X, red_1Y]){
+    systemDraw();
+    ctx.beginPath();
+    ctx.arc(yellowX, yellowY, ballRadius, 0, 2 * Math.PI);
+    ctx.fillStyle = ball.yellow.color;
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(red_1X, red_1Y, ballRadius, 0, 2 * Math.PI);
+    ctx.fillStyle = ball.red_1.color;
+    ctx.fill();
+}
 
 function drawBallPath(){
     var lineColor = "#000000";
-    var a = 200;
+    var a = 500;
     ball.yellow.angle = (ball.yellow.angle+Math.PI*2) % (Math.PI*2);
     //console.log(ball.yellow.angle);
     var endX = ball.yellow.x + Math.cos(ball.yellow.angle) * a;
     var endY = ball.yellow.y + Math.sin(ball.yellow.angle) * a;
     
     systemDraw();
-    ball.yellow.draw();
+    drawBalls([ball.yellow.x, ball.yellow.y, ball.red_1.x, ball.red_1.y]);
     ctx.beginPath();
     ctx.strokeStyle = lineColor; 
     ctx.lineWidth = 1;
@@ -402,84 +496,76 @@ function drawBallPath(){
 }
 drawBallPath();
 
-{//클릭구역 판단
-    function isTable(x,y){//클릭한 구역이 테이블이면 true/아니면 false
-        if((x > table.x && x < table.x+table.width)&&(y > table.y && y < table.y+table.height)){
-            return true;
-        }
-        return false;
-    }
-    function isCuePower(x,y){//클릭한 구역이 큐파워이면 true/아니면 false
-        if((x > cuePower.x && x < cuePower.x+cuePower.width)&&(y > cuePower.y && y < cuePower.y+cuePower.height)){
-            return true;
-        }
-        return false;
-    }
-    function isLeft(x,y){//클릭한 구역이 레프트버튼이면 true/아니면 false
-        if((x > leftButton.x && x < leftButton.x+leftButton.width)&&(y > leftButton.y && y < leftButton.y+leftButton.height)){
-            return true;
-        }
-        return false;
-    }
-    function isRight(x,y){//클릭한 구역이 라이트버튼이면 true/아니면 false
-        if((x > rightButton.x && x < rightButton.x+rightButton.width)&&(y > rightButton.y && y < rightButton.y+rightButton.height)){
-            return true;
-        }
-        return false;
-    }
-    function isBall(x,y){//클릭한 구역이 시네루 버튼이면 true/아니면 false
-        if(((x-ballButton.x)**2 +(y-ballButton.y)**2) < ballButton.radius**2){
-            return true;
-        }
-        return false;
-    }
-}
-function reangle(x,y){//테이블 클릭시 공의 각도를 재지정
-    if((x-ball.yellow.x) > 0){//공의 오른쪽 클릭
-        ball.yellow.angle = Math.asin((y-ball.yellow.y)/Math.sqrt((x-ball.yellow.x)**2 + (y-ball.yellow.y)**2));
-    }
-    else{//공의 왼쪽 클릭
-        ball.yellow.angle = Math.asin(-(y-ball.yellow.y)/Math.sqrt((x-ball.yellow.x)**2 + (y-ball.yellow.y)**2))+Math.PI;
-    }
-    drawBallPath();
-}
-//상태 : ready=준비중(공 치는 준비)  play=게임 표현중(공 움직이는중)
-var state = "ready"
-//표현할 애니메이션 프레임(공들의 위치를 배열의 형태로 가짐)
-var frame = []
-function drawBalls([yellowX, yellowY]){
-    systemDraw();
-    ctx.beginPath();
-    ctx.arc(yellowX, yellowY, ballRadius, 0, 2 * Math.PI);
-    ctx.fillStyle = ball.yellow.color;
-    ctx.fill();
-}
 function moveBalls(){//공들 움직이기
+    //노란공 움직이기
+    var t = 1;//접촉시에 짧은 시간 만큼
     ball.yellow.x += ball.yellow.velocityX;
     ball.yellow.y += ball.yellow.velocityY;
-    if((ball.yellow.x < table.x + ballRadius) || (ball.yellow.x > table.x + table.width - ballRadius)){
+    if((ball.yellow.x > table.x + table.width - ballRadius) || (ball.yellow.x < table.x + ballRadius)){
         ball.yellow.velocityX *= -1;
     }
-    if((ball.yellow.y < table.y + ballRadius) || (ball.yellow.y > table.y + table.height - ballRadius)){
+    if((ball.yellow.y > table.y + table.height - ballRadius) || (ball.yellow.y < table.y + ballRadius)){
         ball.yellow.velocityY *= -1;
     }
-    ball.yellow.velocityX *= 0.9997;
-    ball.yellow.velocityY *= 0.9997;
-    return([ball.yellow.x, ball.yellow.y]);
+    //빨간공1 움직이기
+    ball.red_1.x += ball.red_1.velocityX;
+    ball.red_1.y += ball.red_1.velocityY;
+    if((ball.red_1.x > table.x + table.width - ballRadius) || (ball.red_1.x < table.x + ballRadius)){
+        ball.red_1.velocityX *= -1;
+    }
+    if((ball.red_1.y > table.y + table.height - ballRadius) || (ball.red_1.y < table.y + ballRadius)){
+        ball.red_1.velocityY *= -1;
+    }
+    if(((ball.yellow.x-ball.red_1.x)**2 + (ball.yellow.y-ball.red_1.y)**2) <= ((ballRadius*2)**2)){//노란공 빨간공1 충돌시
+        while((ball.yellow.x-ball.red_1.x)**2 + (ball.yellow.y-ball.red_1.y)**2 <= (ballRadius*2)**2){
+            ball.red_1.x -= ball.red_1.velocityX*0.001;
+            ball.red_1.y -= ball.red_1.velocityY*0.001;
+            ball.yellow.x -= ball.yellow.velocityX*0.001;
+            ball.yellow.y -= ball.yellow.velocityY*0.001;
+            t -= 0.001;
+        }
+        console.log("connect balls")
+        var sinA = (ball.yellow.y-ball.red_1.y) / (2*ballRadius);
+        var cosA = (ball.yellow.x-ball.red_1.x) / (2*ballRadius);
+        console.log(sinA);
+        console.log(cosA);
+        var v_v_1 = (ball.red_1.velocityX * cosA) + (ball.red_1.velocityY * sinA);
+        var v_u_1 = -(ball.red_1.velocityX * sinA)+ (ball.red_1.velocityY * cosA);
+        var v_v_2 = (ball.yellow.velocityX * cosA) + (ball.yellow.velocityY * sinA);
+        var v_u_2 = -(ball.yellow.velocityX * sinA)+ (ball.yellow.velocityY * cosA);
+
+        ball.red_1.velocityX = v_v_2*cosA - v_u_1*sinA;
+        ball.red_1.velocityY = v_v_2*sinA + v_u_1*cosA;
+        ball.yellow.velocityX = v_v_1*cosA - v_u_2*sinA;
+        ball.yellow.velocityY = v_v_1*sinA + v_u_2*cosA;
+    }
+    //테이마의 마찰로인한 감속
+    if(ball.yellow.velocity() != 0){
+        ball.yellow.velocityX -= (ball.yellow.velocityX/ball.yellow.velocity())*fricton_table*t;
+        ball.yellow.velocityY -= (ball.yellow.velocityY/ball.yellow.velocity())*fricton_table*t;
+    }
+    if(ball.red_1.velocity() != 0){
+        ball.red_1.velocityX -= (ball.red_1.velocityX/ball.red_1.velocity())*fricton_table*t;
+        ball.red_1.velocityY -= (ball.red_1.velocityY/ball.red_1.velocity())*fricton_table*t;
+    }
+
+    return([ball.yellow.x, ball.yellow.y, ball.red_1.x, ball.red_1.y]);//노란공, 빨간공 위치 리턴
 }
 function playGame(v){
     ball.yellow.velocityX = Math.cos(ball.yellow.angle) * v;
     ball.yellow.velocityY = Math.sin(ball.yellow.angle) * v;
     var i = 0;
     frame = [];
-    while(ball.yellow.velocity()>0.001){
-        if( i%frameRate==0){
+    while(ball.yellow.velocity()>0.001 || ball.red_1.velocity()>0.001){
+        if(i%frameRate==0){
             frame.push(moveBalls());
         }else{
             moveBalls();
         }
+
         i++;
-        if(i > 20000){
+        if(i > 50000){
+            console.log("over frame limit")
             break;
         }
     }
@@ -488,21 +574,21 @@ function playGame(v){
     let raf; // requestAnimationFrame을 담을 변수
     const performAnimation = () => {
         /* 스타일 조정 스크립트 */
-        drawBalls(frame[i])
+        drawBalls(frame[i]);
         i++;
   
         // 특정한 조건일 경우 raf를 중지하고 콜백 종료
         if(i>=frame.length) {
             cancelAnimationFrame(raf);
             state = "ready";
-            console.log("state turns to ready")
+            console.log("state turns to ready");
             drawBallPath();
             return;
         }
-        raf = requestAnimationFrame(performAnimation) 
+        raf = requestAnimationFrame(performAnimation) ;
         // 함수 내부에서 다시 requestAnimationFrame을 호출하여 반복
     }
-        requestAnimationFrame(performAnimation);
+    requestAnimationFrame(performAnimation);
 }
 
 function clickGame(event){
@@ -511,6 +597,7 @@ function clickGame(event){
   
     if(isTable(mouseX, mouseY)){//테이블 클릭하면 공의 방향 조정
         reangle(mouseX,mouseY);
+        drawBallPath();
     }
     if(isLeft(mouseX, mouseY)){//왼쪽 버튼 누르면 미세하게 왼쪽으로 각도 조절
         ball.yellow.angle -= 0.003;
@@ -521,7 +608,8 @@ function clickGame(event){
         drawBallPath();
     }
     if(isCuePower(mouseX, mouseY)){//큐파워 박스를 누르면 공의 속도를 정하고 시작
-        playGame(1);
+        playGame(((cuePower.x + cuePower.width - mouseX)/cuePower.width));
+        
     }
     if(isBall(mouseX, mouseY)){//볼의 시네루 정하기
         
